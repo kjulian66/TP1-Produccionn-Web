c28b40ac187752d8f8e830c51d7b3b23007de4d9
Kjulian66 <bianchijulian66@gmail.com>
2023-09-25 15:10:22 -0300

    Segundo commit

--- script.js
+++ script.js
@@ -2,10 +2,13 @@
 
 const toggle = document.querySelector(".cnt-nav-toggle");
 const links = document.querySelector(".cnt-nav-links");
-
+const menuGestion = document.querySelector("#cnt-menuGestion");
+const menuGestionCarrito = document.querySelector(".menuGestion-carrito")
 
 toggle.addEventListener("click", function(){
     toggle.classList.toggle("rotate")
     links.classList.toggle("active")
+    menuGestion.classList.toggle("active-menuGestion")
+    menuGestionCarrito.classList.toggle("active-menuGestion-carrito")
 })
 
